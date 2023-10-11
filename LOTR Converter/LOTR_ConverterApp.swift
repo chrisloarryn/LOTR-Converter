30c3a30b98cc99e7f05375a95f944deb87c34fe0
Cristobal Andres Contreras Orellana <ccontreraso@pedrodevaldivia.cl>
2023-10-11 00:46:10 -0300

    Initial Commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by Cristóbal Contreras on 11-10-23.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
