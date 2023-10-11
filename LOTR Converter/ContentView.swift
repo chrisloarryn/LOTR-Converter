7cf9ceb7d6f49ee7d0645d75b48edc3831337a98
Cristobal Andres Contreras Orellana <ccontreraso@pedrodevaldivia.cl>
2023-10-11 01:22:56 -0300

    feat: currency converter basic structure

--- LOTR Converter/ContentView.swift
+++ LOTR Converter/ContentView.swift
@@ -9,13 +9,88 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Background image
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                // Prancing pony image
+                Image("prancingpony")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                // Currency exchange text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                // currency conversion section
+                HStack {
+                    // left conversion section
+                    VStack {
+                        // currency
+                        HStack {
+                            // currency image
+                            Image("silverpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            // currency text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                        }
+                        
+                        // Text field
+                        Text("Text field")
+                    }
+                    
+                    // equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    
+                    // right conversion section
+                    VStack {
+                        // currency
+                        HStack {
+                            // currency text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                            
+                            // currency image
+                            Image("goldpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        
+                        // Text Field
+                        Text("Text field")
+                    }
+                }
+                
+                Spacer()
+                
+                // info button
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        // display exchange info screen
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                    }
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                }
+            }
         }
-        .padding()
     }
 }
 
